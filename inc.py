267e232acb0e906ebd5a7a89e79881f89f85fcaf
黄江斌 <hjbcn@126.com>
2013-04-18 23:24:40 +0800

    允许公开github项目地址

--- inc.py
+++ inc.py
@@ -1,2 +1,2 @@
-epilog = "Report %(prog)s bugs to http://www.truease.com/forum-66-1.html"
-VERSION = "1.0.1"
+epilog = "Report %(prog)s bugs to http://www.truease.com/forum-66-1.html or https://github.com/huangjiangbin/pytools"
+VERSION = "1.0.2"
