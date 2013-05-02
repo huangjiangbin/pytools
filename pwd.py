d455a80cb3c7577d930abba3f4cea98829277530
huangjiangbin <hjbcn@126.com>
2013-05-02 09:10:18 +0800

    pwd模块的标准化改造

--- pwd.py
+++ pwd.py
@@ -1,2 +1,17 @@
 import os
-print( os.getcwd() )
+import argparse
+from inc import EPILOG
+
+def ParseCommandLine():
+    parser = argparse.ArgumentParser(
+        description="show current directory path",
+        epilog=EPILOG,
+        )
+    return parser, parser.parse_args()
+
+def Main():
+    parser, opt = ParseCommandLine()
+    print( os.getcwd() )
+
+if __name__ == '__main__':
+    Main()
