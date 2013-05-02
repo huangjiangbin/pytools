46b0733fb9c57e12341ca2d5fb45a89ac0e835b8
huangjiangbin <hjbcn@126.com>
2013-05-02 13:04:25 +0800

    不使用&copy;符号

--- inc.py
+++ inc.py
@@ -2,4 +2,4 @@
 PROGRAM_TITLE = "pytools powered by truease.com"
 EPILOG = "Report %(prog)s bugs to http://www.truease.com/forum-66-1.html or https://github.com/huangjiangbin/pytools"
 VERSION = "1.0.2"
-COPYRIGHT = "© truease.com 2012-2013"
+COPYRIGHT = "truease.com 2012-2013"
