2104b824afb25b4ca7f7afee4fc9e301f198814c
huangjiangbin <hjbcn@126.com>
2013-05-16 08:59:00 +0800

    func函数库中引入StripCRLF

--- func.py
+++ func.py
@@ -7,3 +7,21 @@ def GetProgramPath():
         return os.sys.executable
     else:
         return os.path.realpath(os.sys.argv[0])
+
+def StripCRLF(s):
+    if not s:
+        return s
+    
+    crlf = "\r\n"
+    cr = "\r"
+    lf = "\n"
+    if isinstance(s, (bytes, bytearray)):
+        crlf = b"\r\n"
+        cr = b"\r"
+        lf = b"\n"
+        
+    if s[-2:] == crlf:
+        return s[:-2]
+    if s[-1:] == lf or s[-1:] == cr:
+        return s[:-1]
+    return s
