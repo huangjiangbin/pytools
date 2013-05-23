eb899d72f17f63d8c91e3106b44ecfca90acd279
huangjiangbin <hjbcn@126.com>
2013-05-23 09:23:00 +0800

    setup文件修正。由于各工具重命名，需要修正setup中相应的生成指令

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 import sys
 from cx_Freeze import setup, Executable
 from inc import *
@@ -29,12 +30,11 @@ setup(
     executables = [
             Executable("pytools-version.py"),
             
+            # 字符串处理
             Executable("cat.py"),
             Executable("tail.py"),
             Executable("head.py"),
-            Executable("file.py"),
             Executable("eval.py"),
-            
             Executable("crlf.py"),
             Executable("strip.py"),            
             Executable("lf.py"),
@@ -46,15 +46,15 @@ setup(
             Executable("lsort.py"),
             Executable("sum.py"),
             
+            # 字符串/数值/信号 生成器
             Executable("pwgen.py"),
             Executable("uuid4.py", targetName="uuid.exe"),
             Executable("urandom.py"),
             Executable("rand.py"),
-            Executable("switch01.py"),
+            Executable("01switch.py"),
+            Executable("ssh-keygen.py"),
             
-            Executable("fstartswith.py"),
-            Executable("fendswith.py"),
-            
+            # 哈希算法
             Executable("b64.py", targetName="base64.exe"),
             Executable("hash.py"),
             Executable("hash.py", targetName="md5.exe"),
@@ -64,42 +64,54 @@ setup(
             Executable("hash.py", targetName="sha384.exe"),
             Executable("hash.py", targetName="sha512.exe"),
             
-            Executable("ssh-keygen.py"),
+            # 文件内容处理
+            Executable("fstartswith.py"),
+            Executable("fendswith.py"),            
+            Executable("file.py"),
             
+            # 系统时间相关程序
             Executable("nowtime.py"),
             Executable("isoweek.py"),
             Executable("uptime.py"),
             Executable("idletime.py"),
+            
+            # 输入控制/流程控制
             Executable("sleep.py"),
             Executable("pbreak.py"),
+            Executable("getchar.py"),
             
+            # 控制台窗口相关
             Executable("cmdsize.py"),
+            Executable("cmdpid.py"),
+            
+            # 系统信息、环境变量
             Executable("pwd.py"),
+            Executable("memfree.py"),
+            Executable("dsnumber.py"),
+            Executable("diskfree.py"),
+            
+            # 文件操作
+            Executable("touch.py"),
             Executable("fs.py"),
             Executable("which.py"),
             Executable("du.py"),
             Executable("fstat.py"),
-            Executable("touch.py"),
             
-            Executable("dsnumber.py"),
-            
-            Executable("ppid.py"),
-            Executable("memfree.py"),
-            Executable("diskfree.py"),
-            
-            Executable("getchar.py"),
-            
+            # 网络客户端
             Executable("ping.py", targetName="yping.exe"),
             Executable("httpd.py"),
             Executable("ftpd.py"),
             Executable("whois.py"),
-            Executable("wget.py"),
+            Executable("urlget.py"),
             Executable("mailto.py"),
             
+            # 图片相关操作
             Executable("iresize.py"),
             
+            # 数据库相关操作
             Executable("mysql-dbrename.py"),
             
+            # 其它特定用途工具
             Executable("css-grid-system.py"),
         ]
 )
