c392802e1aec1bedfdcda436cf8fcc832790c0a4
huangjiangbin <hjbcn@126.com>
2013-05-23 13:58:06 +0800

    sleep工具帮助信息中文化 sleep打印的提示信息可以不换行

--- sleep.py
+++ sleep.py
@@ -1,29 +1,31 @@
+# encoding: utf-8
+import re
 import time
 import argparse
 from inc import EPILOG
 
 def ParseCommandLine():
     parser = argparse.ArgumentParser(
-        description = "sleep some times",
+        description = "休眠指定时间后返回。",
         epilog = EPILOG,
         )
     parser.add_argument(
         "-m", "--millisecond",
         dest="millisecond",
         action="store_true",
-        help="treat N as millisecond"
+        help="以毫秒为单位。"
         )
     parser.add_argument(
         "n",
         nargs=1,
         type=int,
-        help="how many second or millisecond gets to sleep",
+        help="指定休眠的时间。默认单位为秒，如使用了-m选项，则单位为毫秒。",
         )
     parser.add_argument(
         "-i", "--information",
         dest="information",
         action="store",
-        help="output the information before sleep",
+        help="在休眠前，显示提示信息。如需要换行，则使用<br />代替。",
         )
     return parser, parser.parse_args()
 
@@ -34,10 +36,14 @@ def Main():
     if opt.millisecond:
         sleeptime /= 1000.0
     
+    opt.information = re.sub("<br[^<>]*>", "\r\n", opt.information)
     if opt.information:
-        print(opt.information)
+        print(opt.information, end="", flush=True)
     
-    time.sleep(sleeptime)
+    try:
+        time.sleep(sleeptime)
+    except KeyboardInterrupt:
+        pass
     
 if __name__ == '__main__':
     Main()
