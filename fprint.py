24c1eb7feae710daacae034c1919fbbbe448a5ad
huangjiangbin <hjbcn@126.com>
2013-05-24 09:16:28 +0800

    fprint工具改写，去掉空行合并功能

--- fprint.py
+++ fprint.py
@@ -6,26 +6,21 @@ from func import StdoutWrite
 
 def ArgParse():
     parser = argparse.ArgumentParser(
-        description="Concatenate FILE(s), or standard input, to standard output.",
+        description="打印文件内容到标准输出上。不指定输入文件，则从标准输入读取内容。",
         epilog=EPILOG
         )
     parser.add_argument(
         "-n", "--number-lines",
-        dest="number_lines",
+        dest="show_line_number",
         action="store_true",
-        help="number output lines",
-        )    
-    parser.add_argument(
-        "-s", "--squeeze-blank",
-        dest="squeeze_blank",
-        action="store_true",
-        help="suppress repeated empty output lines",
+        help="显示行号",
         )
     parser.add_argument(
         "files",
         metavar="FILE",
         nargs="*",
         default=["-"],
+        help="输入文件",
         )
     return parser, parser.parse_args()
 
@@ -33,42 +28,33 @@ def Main():
     parser, opt = ArgParse()
     
     for f in opt.files:
+        
         if f == "-":
-            fileobj = os.sys.stdin.buffer
+            fileobj = os.sys.stdin.buffer.raw
         else:
             fileobj = open(f, "rb")
         
-        n = 0
-        lastlinebreak = False
-        while 1:
-            n += 1
-            
+        line_number = 0
+        
+        line = b""
+        lastline = b""
+        while True:
+            line_number += 1
             line = fileobj.readline()
             if not line:
                 break
             
-            if opt.squeeze_blank and ( not line.strip() ):
-                if line.endswith(b"\r\n"):
-                    line = b"\r\n"
-                elif line.endswith(b"\r"):
-                    line = b"\r"
-                else:
-                    line = b"\n"
+            if opt.show_line_number:
+                line = ( "%4d "%(line_number) ).encode( "ascii" ) + line
             
-            if opt.number_lines:
-                line = ("%5d "%(n)).encode("ascii") + line
             StdoutWrite(line)
+            lastline = line
+        
+        if not ( lastline.endswith( b"\n" ) or lastline.endswith( b"\r" ) ):
+            print( "" )
             
-            if line.endswith(b"\n") or line.endswith(b"\r"):
-                lastlinebreak = True
-            else:
-                lastlinebreak = False
-                
         if f != "-":
             fileobj.close()
-        
-        if not lastlinebreak:
-            print("")
 
 if __name__ == '__main__':
     Main()
