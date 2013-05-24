1ca46692cebd35d6ef5ac37a270410b4133516ea
黄江斌 <hjbcn@126.com>
2013-05-24 22:46:01 +0800

    fname工具新建（未完成）

---
+++ fname.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+import os
+import shutil
+import argparse
+from inc import EPILOG
+
+def ParseCommandLine():
+    parser = argparse.ArgumentParser(
+        description="文件重命名",
+        epilog=EPILOG,
+        )
+    parser.add_argument(
+        "", "",
+    )
